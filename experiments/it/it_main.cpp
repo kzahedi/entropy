ce81d69a6ec7d1a33edab5a3c0c94fa6e5aeb0a2
CarlottaLanger <lottalanger@web.de>
2016-09-20 13:18:07 +0200

    it_main 2

---
+++ experiments/it/it_main.cpp
@@ -0,0 +1,39 @@
+#include <entropy++/Csv.h>
+#include <entropy++/sparse/MC_MI.h>
+#include <entropy++/sparse/MC_W.h>
+#include <entropy++/sparse/state/MC_W.h>
+#include <entropy++/sparse/state/MC_MI.h>
+#include <entropy++/Matrix.h>
+#include <entropy++/SparseMatrix.h>
+
+#include <time.h>
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include "Feature.h"
+#include "FeatureMatrix.h"
+#include "GIS.h"
+#include "InstanceMatrix.h"
+#include "FeatureMatrixsp.h"
+#include "SCGIS.h"
+#include "GISsp.h"
+#include "Comp.h"
+
+
+
+int main(int argc, char **argv){
+
+DContainer *zX = new DContainer(2,1);
+	*zX << 0 << 1;
+DContainer *zY = new DContainer(2,1);
+	*zY << 0 << 1;
+
+	vector<double> lambda(3);
+		 lambda[0]=1;
+		 lambda[1]=5;
+		 lambda[2]=3;
+//int ColX,int RowX,int ColValY,  vector<double> lambda,DContainer &aX, DContainer &aY,int maxit, double konv,bool time,int seconds)
+		 Comp *test = new Comp(2,100,2,lambda,*zX,*zY,1,0.001,true,10);
+test->comparison();
+}
