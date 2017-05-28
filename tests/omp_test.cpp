ac4c33e65e0eb791bc382f7b1668950c2be4c943
Keyan <keyan.zahedi@gmail.com>
2017-05-27 22:02:35 -0600

    omp test

--- tests/omp_test.cpp
+++ tests/omp_test.cpp
@@ -2,9 +2,22 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
+#include <iostream>
+
+#include <omp.h>
+
+using namespace std;
+
 BOOST_AUTO_TEST_SUITE(OpenMP)
 
 BOOST_AUTO_TEST_CASE(OMP)
 {
+#pragma omp parallel
+  {
+    int threads = omp_get_num_threads();
+    int id = omp_get_thread_num();
+    cout << "hello from thread: " << id << " out of " << threads << endl;
+    sleep(2);
+  }
 }
 BOOST_AUTO_TEST_SUITE_END()
